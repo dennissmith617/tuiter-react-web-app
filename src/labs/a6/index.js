2d71b5c656e53b4ce8d8e8f4e1e9b2089eafd76d
Dennis Smith <dennis.smith@richmond.edu>
2023-02-16 11:19:50 -0500

    A6 in progress. Finished Tuiter portion. Next is Challenge portion.

--- src/labs/a6/index.js
+++ src/labs/a6/index.js
@@ -1,14 +1,17 @@
-import {Link}
-  from "react-router-dom";
+import Classes from "./classes";
+import Styles from "./styles";
+import ConditionalOutput from "./conditional-output";
+import ToDoList from "./todo";
 
 function Assignment6() {
- return(
-  <div>
-   <Link to="/">Lab</Link> |
-   <Link to="/hello">Hello</Link> |
-   <Link to="/tuiter">Tuiter</Link>
-   <h1>Assignment 6</h1>
-  </div>
- )
+ return (
+   <div>
+     <h1>Assignment 6</h1>
+     <ConditionalOutput/>
+     <Styles/>
+     <Classes/>
+     <ToDoList/>
+   </div>
+ );
 }
 export default Assignment6;
