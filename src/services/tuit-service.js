6550ce52fbc4701c2b07f9b7f065a54a223afe21
Dennis Smith <dennis.smith@richmond.edu>
2023-03-23 10:09:14 -0400

    Local Variable changes for Node server

--- src/services/tuit-service.js
+++ src/services/tuit-service.js
@@ -1,5 +1,8 @@
 import axios from 'axios';                                  // import Axios
-const TUITS_API = 'http://localhost:4000/api/tuits';        // location of HTTP services
+//const TUITS_API = 'http://localhost:4000/api/tuits';
+/*const TUITS_API = 'https://tuiter-node-server-app-jx9y.onrender.com/api/tuits';*/
+const API_BASE = process.env.REACT_APP_API_BASE;
+const TUITS_API = `${API_BASE}/tuits`;
 
 export const createTuit = async (tuit) => {
  const response = await axios.post(TUITS_API, tuit)
